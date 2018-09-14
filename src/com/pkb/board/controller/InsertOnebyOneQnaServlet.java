3318036221bf8ed62628599c542314386ec842eb
starm <starm@DESKTOP-ERG1L3R>
2018-09-14 21:22:32 +0900

    eunjin 20180914-2 입력한 폼 컨트롤러에 받기 시도중 ㅠㅠ 사이드 메뉴바에 float:left 추가

--- src/com/pkb/board/controller/InsertOnebyOneQnaServlet.java
+++ src/com/pkb/board/controller/InsertOnebyOneQnaServlet.java
@@ -1,11 +1,14 @@
 package com.pkb.board.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.pkb.board.model.vo.Board;
 
 /**
  * Servlet implementation class InsertOnebyOneQnaServlet
@@ -26,11 +29,16 @@ public class InsertOnebyOneQnaServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String title = request.getParameter("usr");
-		String content = request.getParameter("comment");
+		String title = request.getParameter("onebyonetitle");
+		String content = request.getParameter("onebyonecontent");
 		
 		System.out.println(title);
 		System.out.println(content);
+		
+		Board b = new Board ();
+		
+		b.setArticle_title(title);
+		b.setArticle_contents(content);
 	
 	}
 
