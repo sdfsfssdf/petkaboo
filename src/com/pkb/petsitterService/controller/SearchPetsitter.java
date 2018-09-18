6b54f3121457b2f6ff804b21418d1d356f5eae05
udonmania2018 <winternebel@gmail.com>
2018-09-18 18:35:41 +0900

    1. 펫시터 서비스 등록 관련 부분 전면 변경 준비, 펫시터 서비스 찾기 관련 부분 기초 작업, 메뉴바 변경, 메인 검색 기능 임시 연결
    3. 공정률 60%

---
+++ src/com/pkb/petsitterService/controller/SearchPetsitter.java
@@ -0,0 +1,35 @@
+package com.pkb.petsitterService.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/search.do")
+public class SearchPetsitter extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public SearchPetsitter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String searchKeyword = request.getParameter("searchKeyword");
+		
+		// 테스트코드
+		// System.out.println(searchKeyword);
+		
+		
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
