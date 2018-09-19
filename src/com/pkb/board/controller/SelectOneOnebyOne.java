d0edfcf5678b93338d37ba084289a999f72c092c
starm <starm@DESKTOP-ERG1L3R>
2018-09-19 18:53:30 +0900

    ej-20180919-1 상세조회부분 작성중

--- src/com/pkb/board/controller/SelectOneOnebyOne.java
+++ src/com/pkb/board/controller/SelectOneOnebyOne.java
@@ -1,11 +1,15 @@
 package com.pkb.board.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.pkb.board.model.service.BoardService;
+import com.pkb.board.model.vo.Board;
 
 /**
  * Servlet implementation class SelectOneOnebyOne
@@ -30,6 +34,8 @@ public class SelectOneOnebyOne extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int num = Integer.parseInt(request.getParameter("num"));
 		System.out.println(num);
+		
+		Board b = new BoardService().selectOneOnebyOne(num);
 		
 		
 	}
